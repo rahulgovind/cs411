14e9fe84cd4ad72ba100a8c0314c2bad1f591606
ragnar <rahulgovind517@gmail.com>
2018-11-01 03:14:40 -0500

    Add cors

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask_restful import reqparse
 import jwt
 
 import datetime
+from flask_cors import CORS
 
 
 class Config(object):
@@ -16,6 +17,7 @@ class Config(object):
 
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 app.config.from_object(Config)
 
